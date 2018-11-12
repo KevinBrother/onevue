24dae2c0341ef8870b0063016c04ea5181f3d942
KevinBrother <1301239018@qq.com>
2018-11-12 10:32:12 +0800

    build

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="app">
+    <!-- <Top></Top>  -->
+    
+    <div>
+
+    </div>
+    <!-- <Botton></Botton> -->
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import Top from './components/top'
+// import Bottom from './components/bottom'
+
+export default {
+  name: 'App',
+  components: {
+    Top,
+    HelloWorld,
+  }
+}
+</script>
+
+<style>
+body,div,ol,ul,li,dl,dt,dd,h1,h2,h3,h4,h5,h6,p,form,fieldset,legend,input,img,span {
+	margin:0;
+	padding:0;
+}
+
+#app {
+	overflow-x: hidden;
+	overflow-y: hidden;
+	background-image: url("/static/background.png");
+	background-size: cover;
+}
+
+.hide {
+	display: none;
+}
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style> -->
+
---
+++ src/components/top.vue
@@ -0,0 +1,67 @@
+<template>
+    <div>
+        <div class="top-left"><img src="/static/logo.png"/></div>
+        <div class="top-right">
+            <div class="date">
+                <span id="date"></span>
+            </div>
+            <div class="back-to-index" id="backToIndex">
+                <img src="/static/index.png"/>
+                <div>返回首页</div>
+            </div>
+        </div>
+    </div>    
+</template>
+
+<script>
+export default {
+  name: 'top',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+
+.top-left img {
+	position: absolute;
+	left: 19px;
+	top: 20px;
+	width: 174px;
+	height: 32px;
+}
+
+.top-right {
+	position: absolute;
+	right: 20px;
+	top: 22px;
+	height: 28px;
+	margin-right: 70px;
+}
+
+.top-right .date {
+	font-family: Roboto-Medium;
+	font-size: 20px;
+	font-weight: bold;
+	color: #4A4A4A;
+}
+
+.top-right .back-to-index {
+	position: absolute;
+	top: -14px;
+	right: -75px;
+	font-family: PingFangSC-Regular;
+	font-size: 16px;
+	color: #333333;
+	letter-spacing: 0;
+	text-align: center;
+}
+
+.top-right img {
+	width: 30px;
+	height: 30px;
+}
+</style>
